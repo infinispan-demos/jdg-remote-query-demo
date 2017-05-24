809d0a6017ac45572e1be342b06c4bde45554d08
gnunn1 <gerald.b.nunn@gmail.com>
2017-05-24 12:09:39 -0400

    Update for remote cache

--- src/main/java/org/jboss/infinispan/demo/Config.java
+++ src/main/java/org/jboss/infinispan/demo/Config.java
@@ -2,10 +2,9 @@ package org.jboss.infinispan.demo;
 
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringWriter;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import java.util.Scanner;
 
 import javax.enterprise.inject.Produces;
 
@@ -17,10 +16,7 @@ import org.jboss.infinispan.demo.model.Task;
 import org.infinispan.client.hotrod.marshall.ProtoStreamMarshaller;
 import org.infinispan.protostream.FileDescriptorSource;
 import org.infinispan.protostream.SerializationContext;
-import org.infinispan.protostream.annotations.ProtoSchemaBuilder;
 import org.infinispan.query.remote.client.ProtobufMetadataManagerConstants;
-import org.infinispan.client.hotrod.impl.transport.tcp.FailoverRequestBalancingStrategy;
-import org.infinispan.client.hotrod.configuration.NearCacheMode;
 
 
 /**
@@ -34,7 +30,9 @@ public class Config {
 
     private static final String PROTOBUF_DEFINITION_RESOURCE = "/task.proto";
 
-	private RemoteCacheManager cacheManager; 
+	private RemoteCacheManager cacheManager;
+
+	static Logger log = Logger.getLogger(Config.class.getName());
 	
 	/**
 	 * DONE: Add a default Producer for org.infinispan.client.hotrod.RemoteCache<Long, Task> 
@@ -55,26 +53,31 @@ public class Config {
 		
 	}
 	
-	  /**
-	    * Register the Protobuf schemas and marshallers with the client and then register the schemas with the server too.
-	    */
-	   private void registerSchemasAndMarshallers() throws IOException {
-	      // Register entity marshallers on the client side ProtoStreamMarshaller instance associated with the remote cache manager.
-	      SerializationContext ctx = ProtoStreamMarshaller.getSerializationContext(cacheManager);
-	      ctx.registerProtoFiles(FileDescriptorSource.fromResources(PROTOBUF_DEFINITION_RESOURCE));
-	      ctx.registerMarshaller(new TaskMarshaller());
+	/**
+	* Register the Protobuf schemas and marshallers with the client and then register the schemas with the server too.
+	*/
+	private void registerSchemasAndMarshallers() throws IOException {
+		// Register entity marshallers on the client side ProtoStreamMarshaller instance associated with the remote cache manager.
+		SerializationContext ctx = ProtoStreamMarshaller.getSerializationContext(cacheManager);
+		ctx.registerProtoFiles(FileDescriptorSource.fromResources(PROTOBUF_DEFINITION_RESOURCE));
+		ctx.registerMarshaller(new TaskMarshaller());
 
-          
-	      // register the schemas with the server too
-	      RemoteCache<String, String> metadataCache = cacheManager.getCache(ProtobufMetadataManagerConstants.PROTOBUF_METADATA_CACHE_NAME);
-	   
-	      String errors = metadataCache.get(ProtobufMetadataManagerConstants.ERRORS_KEY_SUFFIX);
-	      
-	      if (errors != null) {
-	         throw new IllegalStateException("Some Protobuf schema files contain errors:\n" + errors);
-	      }
-	       
-	   }
-	  
-
+		
+		// register the schemas with the server too
+		RemoteCache<String, String> metadataCache = cacheManager.getCache(ProtobufMetadataManagerConstants.PROTOBUF_METADATA_CACHE_NAME);
+		//Actually register the proto file
+		String text = new Scanner(Config.class.getResourceAsStream(PROTOBUF_DEFINITION_RESOURCE), "UTF-8").useDelimiter("\\A").next();
+		log.log(Level.INFO, "Registering proto file:\n" + text);
+		try {
+			metadataCache.put("task.proto", text);
+		} catch (Exception e) {
+			log.log(Level.SEVERE, "Error registering proto file");
+		}
+	
+		String errors = metadataCache.get(ProtobufMetadataManagerConstants.ERRORS_KEY_SUFFIX);
+		
+		if (errors != null) {
+			throw new IllegalStateException("Some Protobuf schema files contain errors:\n" + errors);
+		}
+	}
 }
